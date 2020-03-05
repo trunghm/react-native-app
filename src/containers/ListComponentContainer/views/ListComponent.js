667842848f068718de7236f4c8ce019514ae930f
lyhieu <zerohieu@gmail.com>
2020-03-05 15:16:57 +0700

    + delete component not used

--- src/containers/ListComponentContainer/views/ListComponent.js
+++ src/containers/ListComponentContainer/views/ListComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-import {View, ImageBackground, Image, Platform} from 'react-native'
+import {View, ImageBackground, Image, Platform} from 'react-native';
+import ButtonRN from "react-native/Libraries/Components/Button";
 import Images from '../../../themes/Images'
 import {Button,Avatar,SectionHeader} from "../../../components/commons";
 import {GroupButton} from "../../../components/modules";
@@ -17,6 +18,12 @@ export default class ListComponent extends Component {
   render() {
     return (
       <View style={styles().container}>
+        <ButtonRN
+          title ={"Back"}
+          onPress = {()=>{
+            this.props.navigation.goBack();
+          }}
+        />
         <SectionHeader text={"Ex: Button"}/>
         <Button text={"title"}/>
         <SectionHeader text={"Ex: Avatar"}/>
--- src/themes/Colors.js
+++ src/themes/Colors.js
@@ -1,15 +1,7 @@
 export const DARK_MODE = "dark";
 export const LIGHT_MODE = "light";
 
-
-const common = {
-  primaryColor: '#F4F4F4',
-  colorD8: "#D8D8D8",
-  color9B: "#9B9B9B",
-  colorDE: "#DE2A21",
-};
-
-const appColors = {
+export const appColors = {
   primaryBg: [`#FFFFFF`, `#000000`],
   primary: ['#1496bb', '#1496bb'],
   text: [`#333333`, `#FFFFFF`],
@@ -23,7 +15,7 @@ const colors = theme => {
   for (const key in appColors) {
     themeColors[key] = appColors[key][theme === DARK_MODE ? 1 : 0];
   }
-  return {common: common, ...themeColors};
+  return {...themeColors};
 };
 
 export default colors;
