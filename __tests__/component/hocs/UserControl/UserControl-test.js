f53284eb197a69cf0f8b3cfb77e50e12f8c7595f
ruacondilonton <longng@nexlesoft.com>
2020-03-09 16:03:00 +0700

    cm

--- __tests__/component/hocs/UserControl/UserControl-test.js
+++ __tests__/component/hocs/UserControl/UserControl-test.js
@@ -5,11 +5,14 @@
 import 'react-native';
 import React from 'react';
 import UserControl from '../../../../src/components/hocs/UserControl';
+import GroupButton from '../../../../src/components/modules/GroupButton';
 
 // Note: test renderer must be required after react-native.
 import {mount, shallow} from 'enzyme';
 import renderer from  "react-test-renderer";
 import EStyleSheet from 'react-native-extended-stylesheet';
+
+import {Button} from "../../../../src/components/commons/";
 
 
 const originalError = console.error;
@@ -27,11 +30,11 @@ afterAll(() => {
 describe('Testing  UserControl component', () => {
 
   it('renders snapshot as expected', () => {
-    const wrapper = renderer.create(
+    const componentRender = renderer.create(
       <UserControl active={true}/>
     );
 
-    expect(wrapper).toMatchSnapshot();
+    expect(componentRender).toMatchSnapshot();
 
   });
 
@@ -47,6 +50,17 @@ describe('Testing  UserControl component', () => {
     expect( wrapper
       .findWhere((w) => w.prop('text') === 'Active User')
       .first().prop('disabled') ).toEqual(true);
+
+
+    const button1 = wrapper.find(Button).first();
+    expect(button1.prop('text')).toEqual('Deactive User');
+    expect(button1.prop('disabled') ).toEqual(false);
+    const button2 = wrapper.find(Button).last();
+    expect(button2.prop('text')).toEqual('Active User');
+    expect(wrapper.find(GroupButton).prop('index')).toEqual(0);
+    button2.simulate("click");
+    expect(wrapper.find(GroupButton).prop('index')).toEqual(1);
+
 
   });
 
